4a078111e395af39174aaf576a976c49f0a5432d
O'Brien <abucheriobrien@gmail.com>
2024-04-16 21:25:12 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,6 @@
+export default function cleanSet(set, startString) {
+  const values = Array.from(set);
+  const filteredValues = values.filter((value) => value.startsWith(startString));
+  const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
+  return cleanedValues.join('-');
+}
