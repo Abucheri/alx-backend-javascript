8d3857d19c452afee118ee819aecd7615ea96d2f
O'Brien <abucheriobrien@gmail.com>
2024-04-16 21:05:44 +0300

    More set data structure update_0

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  if (!Set.prototype.isPrototypeOf(set) || !Array.isArray(array)) {
-    throw new TypeError('Arguments must be Set and Array');
-  }
   return array.every((element) => set.has(element));
 }
