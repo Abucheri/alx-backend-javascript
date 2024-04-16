1c73cefed4f95c7ef90d62672e22f889de9d839e
O'Brien <abucheriobrien@gmail.com>
2024-04-16 19:32:58 +0300

    More mapping update_0

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(studentsList) {
   if (!Array.isArray(studentsList)) {
     return [];
   }
-  return studentsList.map(student => student.id);
+  return studentsList.map((student) => student.id);
 }
