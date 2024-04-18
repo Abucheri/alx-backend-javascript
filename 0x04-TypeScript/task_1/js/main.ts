c942720f43b603eeab2ae813bf5f47f86e4930cc
O'Brien <abucheriobrien@gmail.com>
2024-04-18 03:39:43 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,15 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -40,3 +49,20 @@ function printTeacher(firstName: string, lastName: string): string {
 }
 
 console.log(printTeacher("John", "Doe"));
+
+class StudentClass implements StudentClass {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student1: StudentClass = new StudentClass('John', 'Doe');
+
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
