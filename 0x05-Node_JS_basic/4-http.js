9b1776c0e4cb9a9dce158ba975ba54068c90621f
O'Brien <abucheriobrien@gmail.com>
2024-06-04 23:45:36 +0300

    Create a small HTTP server using Node's HTTP module update+0

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,19 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 1245;
+
 // Create an HTTP server
 const app = http.createServer((req, res) => {
   // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
   // Send the response body as "Hello Holberton School!"
   res.end('Hello Holberton School!');
 });
 
 // Listen on port 1245
-app.listen(1245);
+app.listen(port, hostname);
 
 module.exports = app;
