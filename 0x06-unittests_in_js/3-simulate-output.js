7f7e83bc7b7f8281cbe3bba1f4b839805fd9f4b0
O'Brien <abucheriobrien@gmail.com>
2024-06-06 19:46:52 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-simulate-output.js
@@ -0,0 +1,5 @@
+const sendPaymentRequestToApi = require('./3-payment');
+
+// Simulate console output for sendPaymentRequestToApi function
+console.log('Sending payment request to API...');
+sendPaymentRequestToApi(100, 20);
